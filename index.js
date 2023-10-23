983d215cd2aa6b818191a9cadae3fd8b6c30e336
Ejiba Sandra Chisom <chisomejiba@gmail.com>
2023-10-23 22:04:18 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const app = () => {
+    return (
+        <div className = "App">
+        </div>
+    )
+}
+
+const Header = () => {
+    return (
+        <header>
+<h1>
+    shopping list
+</h1>
+        </header>
+    )
+}
+// hoisting is the default behaviour of js of moving declarations to the top
+
+export function footer(){
+    return (
+        <footer>
+
+        </footer>
+    )
+}
+export default Header
